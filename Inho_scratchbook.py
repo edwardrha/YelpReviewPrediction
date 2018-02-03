880b9c59cd072987821d6a73b24ae86373aa7acf
InHo Rha <edwardrha@users.noreply.github.com>
2018-02-02 17:03:55 -0800

    scratch

---
+++ Inho_scratchbook.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+df = pd.read_json("dataset/tip.json", lines=True)
+
+df1 = pd.read_json("dataset/business.json", lines=True)
+
+df2 = pd.read_json("dataset/checkin.json", lines=True)
+
+df.head()
+df1.head()
+df2['time'][1]
+df1[df1['business_id'] == 'kREVIrSBbtqBhIYkTccQUg']['hours']
