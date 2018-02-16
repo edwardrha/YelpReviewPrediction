b5291d9e70eadb7dcf37e7594f42db8db0abd00a
InHo Rha <edwardrha@users.noreply.github.com>
2018-02-15 19:57:54 -0800

    An update

--- Inho_scratchbook.py
+++ Inho_scratchbook.py
@@ -1,12 +1,15 @@
 import pandas as pd
 
-df = pd.read_json("dataset/tip.json", lines=True)
+df = pd.read_json("dataset/review.json", lines = True)
 
 df1 = pd.read_json("dataset/business.json", lines=True)
 
 df2 = pd.read_json("dataset/checkin.json", lines=True)
 
+df3 = pd.read_json("dataset/tip.json", lines=True)
+
 df.head()
 df1.head()
+df2.head()
 df2['time'][1]
 df1[df1['business_id'] == 'kREVIrSBbtqBhIYkTccQUg']['hours']
